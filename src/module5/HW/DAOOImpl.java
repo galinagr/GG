c31aa2f7135ca69432ba32d141de33c6990bd34a
galinagr <galina.gryaznova@thomascookonline.com>
2016-10-04 01:11:08 +0300

    Task 5.5

--- src/module5/HW/DAOOImpl.java
+++ src/module5/HW/DAOOImpl.java
@@ -3,20 +3,25 @@ package module5.HW;
 import java.time.Instant;
 import java.util.Date;
 
-public class DAOOImpl implements DAO {
+class DAOOImpl implements DAO {
     @Override
     public Room save(Room room) {
         System.out.println(room.toString() + "saved");
         return room;
+
     }
 
     @Override
     public boolean delete(Room room) {
-        return false;
+        return room.getId() == 101;
     }
 
     @Override
     public Room update(Room room) {
+        while (room.getId() == 101 && room.getPrice() == 400 && room.getPersons() == 2)
+            room.setId(102);
+            room.setPrice(500);
+            room.setPersons(3);
         return null;
     }
 
@@ -24,7 +29,7 @@ public class DAOOImpl implements DAO {
     public Room findById(long id) {
         Room room = new Room(1002, 0, "Kyiv", 2, Date.from(Instant.parse("12-Apr-2016")), "Avrora");
         System.out.println(room.toString() + room.getId() + " is found");
-        return room;
+        return null;
     }
 
     @Override
