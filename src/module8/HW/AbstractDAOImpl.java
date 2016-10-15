a3240a36113e42395fc71bf3e471d6ab571c1cf2
galingr <galinagr@yandex.ru>
2016-10-15 17:18:14 +0300

    8.3-4

--- src/module8/HW/AbstractDAOImpl.java
+++ src/module8/HW/AbstractDAOImpl.java
@@ -7,27 +7,37 @@ public class AbstractDAOImpl implements AbstractDAO<UserDAO> {
 
     @Override
     public UserDAO save(List<UserDAO> list) {
+        list.add(1, new UserDAO(1, "kkk"));
         System.out.println(list.toString() + "saved");
         return null;
     }
 
     @Override
     public void delete(List<UserDAO> list) {
-
+        list.remove(0);
     }
 
     @Override
     public void deleteAll(List<UserDAO> list) {
+        list.clear();
 
     }
 
     @Override
     public void saveAll(List<UserDAO> list) {
+        for (int i = 0; i < list.size(); i++) {
+
+            if (list.get(i).getId() > 12)
+                list.set(i, new UserDAO(i, "gdg")).setId(12);
+        }
 
     }
 
     @Override
     public List<UserDAO> getList(List<UserDAO> list) {
-        return null;
+        list.get(0);
+        list.get(1);
+        list.get(3);
+        return list;
     }
 }
